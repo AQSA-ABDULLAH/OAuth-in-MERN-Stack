0f0073b3eec016f51e98c21a64178b801a2b28dd
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-11-05 17:12:10 +0500

    apply middleware before signup and login

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,15 +1,4 @@
-// const express = require('express');
-// const UserRegistration = require('../controllers/userRegistration');
-// const { userLogin } = require('../controllers/userLogin');
-// const router = express.Router();
-// require("../db/connection");
 
-// // PUBLIC ROUTES
-// router.post("/user-signUp", UserRegistration.userRegistration);
-// router.post("/user-signIn", userLogin);
-// router.post("/verify-otp", UserRegistration.verifyOTP);
-
-// module.exports = router;
 const express = require('express');
 const { userRegistration, verifyOTP } = require('../controllers/userRegistration');
 const { userLogin } = require('../controllers/userLogin');
@@ -17,9 +6,9 @@ const { signupValidation, loginValidation } = require('../middlewares/AuthValida
 
 const router = express.Router();
 
-router.post('/signup', signupValidation, userRegistration); // Registration route
-router.post('/login', loginValidation, userLogin);          // Login route
-router.post('/verify-otp', verifyOTP);                      // OTP Verification route
+router.post('/signup', signupValidation, userRegistration);
+router.post('/login', loginValidation, userLogin);         
+router.post('/verify-otp', verifyOTP);                  
 
 module.exports = router;
 
