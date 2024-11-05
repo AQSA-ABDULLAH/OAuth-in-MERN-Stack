7a0414104cdcae96209841111c61e604c091ac0f
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-11-05 09:39:08 +0500

    create routes folder

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,13 +1,26 @@
 const express = require('express');
-const { UserController } = require('../controllers/UserController'); 
+const { UserController } = require('../controllers/users/userRegistration'); 
+const { userLogin } = require('../controllers/users/userLogin');
+const {ProfileController} = require("../controllers/users/userProfile")
+const {ForgetPasswordController} = require("../controllers/users/forgetPassword")
 const router = express.Router();
 require("../db/connection");
 
 // PUBLIC ROUTES
-router.post("/create-user", UserController.userRegistration);
-router.get("/get-users", UserController.getAllUsers);
-router.get("/get-user-by-id/:id", UserController.getUserId);
-router.patch("/update-user/:id", UserController.updateUser);
-router.delete("/delete-user/:id", UserController.deleteUser);
+router.post("/user_signUp", UserController.userRegistration);
+router.post("/user_signIn", userLogin);
+
+// PROFILE ROUTES
+router.get("/get-user-profile/:id", ProfileController.getUserProfile);
+router.get("/get-users", ProfileController.getAllUsers);
+router.patch("/update-profile/:id", ProfileController.updateUserProfile);
+
+// FORGET PASSWORD ROUTES
+router.post("/forget-password", ForgetPasswordController.forgetPassword )
+router.post("/reset-pasword", ForgetPasswordController.resetPassword )
+router.patch("/update-password", ForgetPasswordController.updatePassword);
+
+// OTP ROUTES
+router.post("/send-otp", UserController.verifyOTP )
 
 module.exports = router;
