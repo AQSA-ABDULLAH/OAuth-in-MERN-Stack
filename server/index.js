0d34fa138a4579032a70b56e15ffc60f74f9c471
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-11-06 14:54:23 +0500

    comit

--- server/index.js
+++ server/index.js
@@ -3,30 +3,29 @@ const cors = require("cors");
 require("dotenv").config();
 
 const port = process.env.PORT || 3000;
-const frontend_url = process.env.FRONTEND_URL || 'https://o-auth-in-mern-stack-frontend-78x.vercel.app'; 
 const app = express();
 
-app.use(cors({
-    origin: frontend_url,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+// Enable CORS for all routes and origins
+app.use(cors());
 
+// Middleware to parse JSON bodies
 app.use(express.json());
 
-// Database connection
+// Connection to MongoDB
 require('./db/connection');
 
 // Routes
 const userRoutes = require('./routes/users');
+
+// Load Routes
 app.use('/api/user', userRoutes);
 
 // Define a simple route
 app.get("/", (req, res) => {
-    res.send("Welcome to drak!");
+    res.send("Hello, World!");
 });
 
-// Vercel serverless function export
-module.exports = app;
-
-
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
